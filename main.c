b7aacc88472cff1dbbc59895e9f0a56e28a1e5d7
Ekremunalytu <ekremunalw1@gmail.com>
2023-01-16 17:24:02 +0300

    Struct has buit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    printf("Welcome to Library");
+
+    struct Book{
+        Name;
+        Author;
+        Publish_Year;
+        Page;
+    }
+
+    
+
+
+
+
+
+
+    return 0;
+}
