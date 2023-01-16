f57a61924c8d55f99602b61ae5fcdb1a84a2232a
Ekremunalytu <ekremunalw1@gmail.com>
2023-01-16 17:53:09 +0300

    switch added

--- main.c
+++ main.c
@@ -1,19 +1,42 @@
 #include <stdio.h>
 
 int main() {
-    printf("Welcome to Library");
+    printf("Welcome to Library\n");
 
     struct Book{
-        Name;
-        Author;
-        Publish_Year;
-        Page;
-    }
+        char Name[20];
+        char Author[20];
+        int Publish_Year;
+        int Page;
+        int Is_Avalible;
+    };
 
+    printf("Please search for the book you want\n" );
+    int search;
+    printf("For name search press 1 ");
+    printf("\nFor Author search press 2 ");
+    printf("\nFor Publish year search press 3 ");
+    printf("\nFor page search search press 4 \n");
     
+    scanf("%d", &search);
 
+    switch (search){
+        case 1:
+            printf("it works");
+            break;
+        case 2:
+            printf("it works");
+            break;
+        case 3:
+            printf("it works");
+            break;
+        case 4:
+            printf("it works");
+            break;
+        default:
+            printf("Error");
 
-
+    }
 
 
 
